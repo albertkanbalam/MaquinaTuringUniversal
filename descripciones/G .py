d5914a3950ad2d19049d53bc137c295ea01d58bf
AlanMonRa <108620333+AlanMonRa@users.noreply.github.com>
2023-02-16 19:47:13 -0600

    Commit2

--- descripciones/G .py
+++ descripciones/G .py
@@ -1,11 +1,11 @@
-{
-    "Estados" :["q0", "qA", "qB", "qC"],
-    "Entrada" :[ "a", "b" ],
-    "Cinta" :[ ["a"] , ["b"], ["_"], ["X"]],
-    "Inicial" :"q0",
-    "Blanco" :"_",
-    "Finales" :["qf"],
-    "Transiciones" :[
+descripcionG={
+    "Estados": ["q0", "qA", "qB", "qC"],
+    "Entrada": [ "a", "b" ],
+    "Cinta": [ ["a"] , ["b"], ["_"], ["X"]],
+    "Inicial": "q0",
+    "Blanco": "_",
+    "Finales": ["qf"],
+    "Transiciones": [
 	["q0", ["_"] ,"q0", ["a"] ,"R"],
 	["q0", ["_"] ,"q0", ["b"] ,"R"]
     ]
